442a0185da2ef4cf3653bc2a08373f91e714acc4
fchaoyaoeng <fchaoyaoeng@gmail.com>
2026-03-03 15:09:51 +0800

    chore: 更新關於頁面程式碼行數統計 15,146行 (70檔)

--- pages/about.php
+++ pages/about.php
@@ -31,9 +31,9 @@
             <tr>
                 <th>程式碼行數</th>
                 <td>
-                    <strong>14,581</strong> 行
+                    <strong>15,146</strong> 行
                     <span style="color:#888;font-size:0.85rem;margin-left:8px;">
-                        (.php: 12,929 &nbsp;|&nbsp; .css: 968 &nbsp;|&nbsp; .js: 684)
+                        (70 個檔案：.php: 13,494 &nbsp;|&nbsp; .css: 968 &nbsp;|&nbsp; .js: 684)
                     </span>
                     <br><small style="color:#aaa;">統計日期：2026-03-03</small>
                 </td>
